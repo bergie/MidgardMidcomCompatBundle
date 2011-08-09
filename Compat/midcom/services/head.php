2158f21b6c71391690fa26eabe8e89fe508470d6
flack <flack@contentcontrol-berlin.de>
2011-08-09 10:08:51 -0700

    Fix notice

--- Compat/midcom/services/head.php
+++ Compat/midcom/services/head.php
@@ -15,6 +15,9 @@ class midcom_services_head
 
     public function enable_jquery($version = null)
     {
-
+        if (!defined('MIDCOM_JQUERY_UI_URL'))
+        {
+            define('MIDCOM_JQUERY_UI_URL', MIDCOM_STATIC_URL . "/jQuery/jquery-ui-{$GLOBALS['midcom_config']['jquery_ui_version']}");
+        }
     }
 }
