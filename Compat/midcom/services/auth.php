ad435844c99aaabd70d1808ae725a3131ac5ef8a
Henri Bergius <henri.bergius@iki.fi>
2011-08-10 13:02:54 +0300

    Add more auth API

--- Compat/midcom/services/auth.php
+++ Compat/midcom/services/auth.php
@@ -7,9 +7,15 @@ class midcom_services_auth extends ContainerAware
 {
     public function require_do($privilege, $object, $message = null)
     {
-        if (!$this->can_do($privilege, $object)) {
-            throw new AccessDeniedException($message);
+        if ($this->can_do($privilege, $object)) {
+            return;
         }
+
+        if (!$message) {
+            $message = $this->container->get('translator')->trans('access denied: privileges required', array(), 'midcom');
+        }
+
+        throw new AccessDeniedException($message);
     }
 
     public function can_do($privilege, $object = null)
@@ -19,6 +25,34 @@ class midcom_services_auth extends ContainerAware
         } catch (AuthenticationCredentialsNotFoundException $e) {
             return false;
         }
+    }
+
+    public function is_valid_user()
+    {
+        $token = $this->container->get('security.context')->getToken();
+        if (!$token) {
+            return false;
+        }
+        return true;
+    }
+
+    public function require_valid_user($method = 'form')
+    {
+        if (!$this->is_valid_user()) {
+            $message = $this->container->get('translator')->trans('authentication required', array(), 'midcom');
+            throw new AccessDeniedException($message);
+        }
+    }
+
+    public function require_admin_user($message = null)
+    {
+        if ($this->admin) {
+            return;
+        }
+        if (!$message) {
+            $message = $this->container->get('translator')->trans('access denied: admin level privileges required', array(), 'midcom');
+        }
+        throw new AccessDeniedException($message);
     }
 
     public function __get($key)
