715a14e94e7cec5f64d3570035bab8e805dead35
Henri Bergius <henri.bergius@iki.fi>
2011-08-19 19:37:45 +0300

    Move permalink resolution to the permalinks service

--- Compat/midcom/services/permalinks.php
+++ Compat/midcom/services/permalinks.php
@@ -1,9 +1,32 @@
 <?php
 
 use Symfony\Component\DependencyInjection\ContainerAware;
+use Midgard\MidcomCompatBundle\Bundle\ComponentBundle;
+use midgard_query_builder as QB;
 
 class midcom_services_permalinks extends ContainerAware
 {
+    public function resolve_permalink($guid)
+    {
+        $qb = new QB('midgard_topic');
+        $topics = $qb->execute();
+
+        $bundles = $this->container->get('kernel')->getBundles();
+        foreach ($bundles as $bundle) {
+            if (!$bundle instanceof ComponentBundle) {
+                continue;
+            }
+
+            foreach ($topics as $topic) {
+                $uri = $bundle->resolvePermalink($topic, $guid);
+                if ($uri) {
+                    return $uri;
+                }
+            }
+        }
+        return null;
+    }
+
     public function create_permalink($guid)
     {
         return $this->container->get('router')->getGenerator()->generate('_midcom_permalink', array('guid' => $guid));
--- Controller/PermalinkController.php
+++ Controller/PermalinkController.php
@@ -3,27 +3,16 @@ namespace Midgard\MidcomCompatBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use midgard_query_builder as QB;
-use Midgard\MidcomCompatBundle\Bundle\ComponentBundle;
 
 class PermalinkController extends Controller
 {
     public function redirectAction($guid)
     {
-        $qb = new QB('midgard_topic');
-        $topics = $qb->execute();
-
-        $bundles = $this->container->get('kernel')->getBundles();
-        foreach ($bundles as $bundle) {
-            if (!$bundle instanceof ComponentBundle) {
-                continue;
-            }
-
-            foreach ($topics as $topic) {
-                $uri = $bundle->resolvePermalink($topic, $guid);
-                if ($uri) {
-                    return $this->redirect($uri);
-                }
+        if (class_exists('midcom')) {
+            $uri = \midcom::get()->permalinks->resolve_permalink($guid);
+            var_dump($uri);
+            if ($uri) {
+                return $this->redirect($uri);
             }
         }
 
